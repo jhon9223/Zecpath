cd4e848d787d9b63d2db4b959b06ed34c9eb9551
jhon2293 <johnnj2297@gmail.com>
2026-07-23 22:58:58 +0530

    Test feature branch

---
+++ accounts/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    # Add any additional fields you want for your custom user model
+    ADMIN = "ADMIN"
+    EMPLOYER = "EMPLOYER"
+    CANDIDATE = "CANDIDATE"
+    ROLE_CHOICES = [
+        (ADMIN, "Admin"),
+        (EMPLOYER, "Employer"),
+        (CANDIDATE, "Candidate"),
+    ]
+
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+
+class CandidateProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user.email
+
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user.email
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_ROOT = BASE_DIR / 'media'
+AUTH_USER_MODEL = "accounts.User"
