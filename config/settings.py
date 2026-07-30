a64f52a1052a244824b84a1bd2de522309185b5f
jhon2293 <johnnj2297@gmail.com>
2026-07-30 22:58:31 +0530

    day13v2

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+
+    "EXCEPTION_HANDLER": "config.exceptions.custom_exception_handler",
 }
 
 SIMPLE_JWT = {
