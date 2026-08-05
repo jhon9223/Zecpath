baa21af2b67d36c603212c53d15efec3d20c4e99
John N J <https://github.com/jhon9223>
2026-08-05 22:28:49 +0530

    17

--- jobs/models.py
+++ jobs/models.py
@@ -66,6 +66,7 @@ class Job(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     updated_at = models.DateTimeField(auto_now=True)
+    is_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
