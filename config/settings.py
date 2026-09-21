2b932507f699be13a0d9e43119f5d5bbf16083b2
John N J <johnnj2297@gmail.com>
2026-09-21 21:00:53 +0530

    Prepare Django settings for deployment

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,8 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
