bf305412fda9d002f4f809e0135937c444e718fb
John N J <johnnj2297@gmail.com>
2026-09-21 21:47:05 +0530

    Configure static files for deployment

--- config/settings.py
+++ config/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-AUTH_USER_MODEL = "accounts.User"
 
 
 REST_FRAMEWORK = {
