f0db03315e8f59d15322bab3f424863f04d69f29
John N J <johnnj2297@gmail.com>
2026-09-26 20:52:57 +0530

    Day 55: Harden production security settings

--- config/settings.py
+++ config/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
-
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "3.106.184.104,localhost,127.0.0.1"
+).split(",")
 
 # Application definition
 
